17c019a91949e6d35dabd9811c284b7b0cba1cec
zk-313 <zulfiqar991@hotmail.com>
2024-10-18 23:32:14 -0400

    updated website and created basic outline

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const header = document.getElementById("headerstuff");
 const content = document.getElementById("content");
 
  // Get the navbar height
-const headerHeight = header.offsetHeight;
-
+let headerHeight = header.offsetHeight;
  // Apply the height dynamically as padding to the body or content
-content.style.paddingTop = headerHeight + "px";
+content.style.paddingTop = (headerHeight-100) + "px";
+
+window.addEventListener("resize", function() {
+    // Get the navbar height
+    let headerHeight = header.offsetHeight;
+    // Apply the height dynamically as padding to the body or content
+    content.style.paddingTop = (headerHeight-100) + "px";
+});
 
 window.addEventListener("scroll", function() {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
