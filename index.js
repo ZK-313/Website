61659e40f33341131c0f2cd20399f6ce42b45c09
zk-313 <zulfiqar991@hotmail.com>
2024-11-21 10:55:46 -0500

    Updated project information

--- index.js
+++ index.js
@@ -33,5 +33,13 @@ function proj1(){
     const description = document.getElementById("project_info");
     document.getElementById("project_name").innerHTML = "Blackjack";
     description.innerHTML = "A simple blackjack game made in Python utilizing the tkinter library for GUI, and Pillow (PIL) to handle images. The game has a special ruleset which can be found in the 'rules' window which outlines how bets will be returned.";
+    document.getElementById("git-link").setAttribute("href","");
+    //description.innerHTML = description.innerHTML + "<iframe src=\"https://replit.com/@ZulfiqarK/Blackjack-tkinta5?lite=1&outputonly=1\" width=\"600\" height=\"400\"></iframe>";
+}
+function proj2(){
+    const description = document.getElementById("project_info");
+    document.getElementById("project_name").innerHTML = "Todo-List CLI / App";
+    description.innerHTML = "A simple Java command line interface to simulate a todo list. Allows users to add and remove tasks, along with mark them as done and incomplete. All commands can be found by typing 'todo -h' in the terminal. There is also a frontend app for Windows users who choose to use it, made using HTML, css, and javascript, all powered by ElectronJS.";
+    document.getElementById("git-link").setAttribute("href","https://github.com/ZK-313/Todo-list-CLI");
     //description.innerHTML = description.innerHTML + "<iframe src=\"https://replit.com/@ZulfiqarK/Blackjack-tkinta5?lite=1&outputonly=1\" width=\"600\" height=\"400\"></iframe>";
 }
